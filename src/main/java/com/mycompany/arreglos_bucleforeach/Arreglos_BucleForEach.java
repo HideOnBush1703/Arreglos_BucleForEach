de90f675f25182a299b1bbba0cdfe033cee23382
Carlos <estarlort1703@gmail.com>
2024-04-17 18:41:21 -0600

    Netbeans

---
+++ src/main/java/com/mycompany/arreglos_bucleforeach/Arreglos_BucleForEach.java
@@ -0,0 +1,27 @@
+
+package com.mycompany.arreglos_bucleforeach;
+
+public class Arreglos_BucleForEach {
+
+    public static void main(String[] args) {
+        
+        String[] nombres={"Josue","Chema","Don","Cholo","Dik","Step","Michel","Isa","Curie"};
+    
+    for(String i:nombres){
+        System.out.println("Nombres:"+i);
+    }    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
